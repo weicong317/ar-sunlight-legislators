6421956a62ccd015ca977b15f7f91dff0174d237
mxchan <mingxiangchan@gmail.com>
2016-06-23 14:08:06 +0800

    added config to autoload models

--- db/config.rb
+++ db/config.rb
@@ -1,3 +1,10 @@
 require 'active_record'
 
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => "#{File.dirname(__FILE__)}/../db/ar-sunlight-legislators.sqlite3")
+
+APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+
+Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
+  filename = File.basename(model_file).gsub('.rb', '')
+  autoload ActiveSupport::Inflector.camelize(filename), model_file
+end
